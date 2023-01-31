2f5a022a1a1e9ce7d62c3907f2c6891d404a0f03
rogervalentim <valentim.roger33@gmail.com>
2023-01-30 23:14:28 -0300

    add articles list

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,7 +1,17 @@
+import { useParams } from 'react-router-dom';
+import articles from './article-content';
 
 const ArticlePage = () => {
+  const { articleId } = useParams();
+  const article = articles.find(article => article.name === articleId)
+  
   return (
-    <div>This is ArticlePage</div>
+    <>
+    <h1>{article.title}</h1>
+    {article.content.map((paragraph, i) => (
+      <p key={i}>{paragraph}</p>
+    ))}
+    </>
   )
 }
 
--- src/pages/ArticlesListPage.jsx
+++ src/pages/ArticlesListPage.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import articles from "./article-content";
 
 const ArticlesListPage = () => {
   return (
-    <div>ArticlesListPage</div>
+    <>
+    <h1>Articles</h1>
+    {articles.map(article => (
+      <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
+        <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0, 150)}...</p>
+      </Link>
+    ))}
+    </>
   )
 }
 
