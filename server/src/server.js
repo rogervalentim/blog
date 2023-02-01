7affe930ba51082d6336c1a982313dd7519b9a1d
rogervalentim <valentim.roger33@gmail.com>
2023-01-31 22:53:46 -0300

    add comments

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,51 @@
 import express from "express";
 
-const app = express();
+let articlesInfo = [
+    {
+        name: 'learn-react',
+        upvotes: 0,
+        comments: [],
+    },
+    {
+        name: 'learn-node',
+        upvotes: 0,
+        comments: [],
+    },
+    {
+        name: 'learn-mongodb',
+        upvotes: 0,
+        comments: [],
+    }
+]
 
-app.get('/hello', (req, res) => {
-    res.send("hello world!");
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
+});
+
+app.post('/api/articles/:name/comments', (req,res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist!');
+    }
 });
 
 app.listen(8000, () => {
-    console.log('Server is listening on port 8000')
-})
+    console.log('Server is listening on port 8000');
+});
