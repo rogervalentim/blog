911e298a3b1c88e09192e1dd448c726c87638378
rogervalentim <valentim.roger33@gmail.com>
2023-02-08 22:35:02 -0300

    add axios

--- web/src/pages/ArticlePage.jsx
+++ web/src/pages/ArticlePage.jsx
@@ -2,16 +2,26 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
 
+import axios from 'axios';
+
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
   const [ articleInfo, setArticleInfo ] = useState({ upvotes: 0, comments: []});
-
-  useEffect(() => {
-    setArticleInfo({ upvotes: 3, comments: [] });
-  }, []) 
   const { articleId } = useParams();
+  
+  useEffect( () => {
+    const loadArticleInfo = async () => {
+    const response = await axios.get(`/api/articles/${articleId}`);
+    const newArticleInfo = response.data;
+    setArticleInfo(newArticleInfo);
+    }
+
+    loadArticleInfo();
+  }, []) 
+
   const article = articles.find(article => article.name === articleId);
+  
 
   if (!article) {
     return <NotFoundPage />
